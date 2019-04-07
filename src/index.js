a3e12113e64e90996fb5953804ef81d49278c75e
Carlton Joseph <carlton.joseph@gmail.com>
2019-04-07 00:39:07 -0700

    add basic history

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Square = () => <button className="square">{/* TODO */}</button>;
+const Square = ({ value, clicked }) => {
+  return (
+    <button className="square" onClick={clicked}>
+      {value}
+    </button>
+  );
+};
 
-const Board = () => {
-  const renderSquare = i => <Square />;
-  const status = "Next player: X";
+const Board = ({ val, clicked }) => {
+  const renderSquare = i => (
+    <Square value={val[i]} clicked={() => clicked(i)} />
+  );
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -30,17 +36,38 @@ const Board = () => {
   );
 };
 
-const Game = () => (
-  <div className="game">
-    <div className="game-board">
-      <Board />
+const Game = () => {
+  const [val, setVal] = useState(Array(9).fill(""));
+  const [nextPlayer, setNextPlayer] = useState("X");
+  const [history, setHistory] = useState([]);
+
+  const clicked = i => {
+    setVal(val.map((item, idx) => (idx === i ? nextPlayer : item)));
+    setHistory([...history, { position: i, value: nextPlayer }]);
+    setNextPlayer(nextPlayer === "X" ? "O" : "X");
+  };
+
+  const moves = history.map(({ position, value }, idx) => {
+    console.log(idx, position, value);
+    return (
+      <li key={position}>
+        {idx}, {position}, {value}
+      </li>
+    );
+  });
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board val={val} clicked={clicked} />
+      </div>
+      <div className="game-info">
+        <div className="status">Next player: {nextPlayer}</div>
+        {console.log(moves)}
+        <ol>{moves}</ol>
+      </div>
     </div>
-    <div className="game-info">
-      <div>{/* status */}</div>
-      <ol>{/* TODO */}</ol>
-    </div>
-  </div>
-);
+  );
+};
 
 // ========================================
 
